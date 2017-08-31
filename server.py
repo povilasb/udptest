76974afc71ab1b844b514f5912060cb38177b4d3
Povilas Balciunas <balciunas90@gmail.com>
2017-08-31 10:50:58 +0300

    Add method to stop the server

--- server.py
+++ server.py
@@ -9,15 +9,20 @@ class Server:
         self._port = port
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._tasks = curio.Queue()
+        self._keep_running = True
 
     async def run(self) -> None:
         self._sock.bind(('0.0.0.0', self._port))
         await curio.spawn(join_tasks, self._tasks)
 
         async with self._sock:
-            while True:
+            while self._keep_running:
                 data, addr = await self._sock.recvfrom(4096)
                 await self._on_data(data, addr)
+
+    async def stop(self) -> None:
+        self._keep_running = False
+        await self._sock.close()
 
     async def _on_data(self, data: bytes, addr: Tuple[str, int]) -> None:
         task = await curio.spawn(self._sock.sendto, b'response', addr)
